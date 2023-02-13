ab7c784ca5f247c354de9ee15f92a1627f000a5c
AlexKomz <a.komzoltsev@gmail.com>
2023-02-13 19:15:59 +0300

    Add tsc & eslint with hello world on fastyfy

---
+++ apps/server/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  rules: {}
+}
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world!')
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+const start = async (): Promise<void> => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+await start()
